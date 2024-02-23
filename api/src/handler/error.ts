1e9f0dd622e4736318330e156cd6aaa6b7cca864
Erik Golinelli <erik@codekraft.it>
2024-02-23 22:00:43 +0100

    response handlers

---
+++ api/src/handler/error.ts
@@ -0,0 +1,31 @@
+import { FastifyReply } from 'fastify'
+import {ResponseInterface} from "../model/types.js";
+
+
+const reply = async (replyponseData: ResponseInterface, reply: FastifyReply) => {
+
+  reply.header('Content-Type', 'application/json;charset=utf-8').code(200)
+
+  if ('error' in replyponseData) {
+    switch (replyponseData.error.code) {
+      case 400: {
+        reply.header('Content-Type', 'application/json;charset=utf-8').code(replyponseData.error.code)
+        break
+      }
+      case 403: {
+        reply.header('Content-Type', 'application/json;charset=utf-8').code(replyponseData.error.code)
+        break
+      }
+      case 404: {
+        reply.header('Content-Type', 'application/json;charset=utf-8').code(replyponseData.error.code)
+        break
+      }
+      default: {
+        reply.header('Content-Type', 'application/json;charset=utf-8').code(500)
+        break
+      }
+    }
+  }
+  //  return responseCode
+}
+export default { reply }
---
+++ api/src/handler/handler.ts
@@ -0,0 +1,20 @@
+import { FastifyReply } from 'fastify'
+import parseResponse from "./response.js";
+import errorHandler from "./error.js";
+import { ResponseInterface } from "../model/types.js";
+
+export const responseSender = async (data: ResponseInterface, reply: FastifyReply): Promise<void> => {
+    await errorHandler.reply(data, reply)
+    reply.send(data)
+}
+
+const responseHandler = async (next: Function, reply: FastifyReply): Promise<void> => {
+    try {
+        const data: ResponseInterface = await next()
+        return responseSender(parseResponse(data), reply)
+    } catch (error) {
+        return responseSender(parseResponse(error), reply)
+    }
+}
+
+export default responseHandler
