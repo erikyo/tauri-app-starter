5d89caff25e1b3353347ab3581c476390bdb309b
Erik Golinelli <erik@codekraft.it>
2024-02-24 22:13:44 +0100

    register fastify/swagger (with ui)

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,19 +2,23 @@ import Fastify, { type FastifyInstance } from "fastify";
 
 import config from "./plugins/config.js";
 import auth from "./plugins/auth.js";
+import swagger from "./plugins/swagger.js";
+import swaggerUi from "./plugins/swagger-ui.js";
 import db from "./adapters/mysql.js";
+
 import routes from "./routes/index.js";
 
 export default async function appFramework(): Promise<FastifyInstance> {
   return (
     Fastify({ logger: true })
       .register(config)
+      .register(swagger)
+      .register(swaggerUi)
       .register(auth)
       .register(db)
 
       // Register Routes
       .register(routes)
-      //.register(todoRoutes)
 
       .ready()
   );
