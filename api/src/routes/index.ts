f7ed99fe4eb6699612134196923839f25ca56f85
Erik Golinelli <erik@codekraft.it>
2024-02-24 22:12:49 +0100

    basic routes

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,15 +1,15 @@
-import fastifyPlugin from 'fastify-plugin'
-import Session from 'supertokens-node/recipe/session/index.js'
+import fastifyPlugin from "fastify-plugin";
+import Session from "supertokens-node/recipe/session/index.js";
 
-async function routes (server, options) {
-    server.get('/', () => {
-        return 'hello world '
-    })
+async function routes(server, options) {
+  server.get("/", () => {
+    return "hello";
+  });
 
-    server.get('/authorized/', async (request, reply) => {
-        const session = await Session.getSession(request, reply)
-        return session ? { message: 'authorized' } : { message: 'unauthorized' }
-    })
+  server.get("/authorized/", async (request, reply) => {
+    const session = await Session.getSession(request, reply);
+    return session ? { message: "authorized" } : { message: "unauthorized" };
+  });
 }
 
-export default fastifyPlugin(routes)
+export default fastifyPlugin(routes);
