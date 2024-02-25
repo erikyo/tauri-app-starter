0274f00f14dc02ccfc1b1f43bc18da5c2e387645
Erik Golinelli <erik@codekraft.it>
2024-02-25 23:27:12 +0100

    todo routes auth

--- api/src/routes/todo.ts
+++ api/src/routes/todo.ts
@@ -1,25 +1,27 @@
-import { FastifyInstance, FastifyPluginOptions, FastifyReply } from "fastify";
+import { FastifyInstance, FastifyPluginOptions } from "fastify";
 import fastifyPlugin from "fastify-plugin";
 import { MySQLPromisePool } from "@fastify/mysql";
 import { Todo } from "../schema/todo.schema.js";
 import { TodoValidationSchema } from "../schema/todo.validation.js";
+import { sessionHandler } from "../handler/handler.js";
 
 const prefix = "/task";
 
 async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
   const connection = await (fastify.mysql as MySQLPromisePool).getConnection();
+
   /**
    * Route for getting all todos
    */
-  fastify.get(prefix, async (req, reply) => {
+  fastify.get(prefix, { preHandler: sessionHandler }, async (req, res) => {
     /**
      * The TodoModel instance
      */
-    const [rows] = await connection.query("SELECT * FROM todo");
-    return rows;
+    const result = await connection.query("SELECT * FROM todo");
+    return result[0] as Todo[];
   });
 
-  fastify.post(prefix, TodoValidationSchema, async (req, reply) => {
+  fastify.put(prefix, { preHandler: sessionHandler }, async (req, reply) => {
     const todo = req.body as Todo;
     const result = await connection.query("INSERT INTO todo SET ?", [todo]);
     return (
@@ -31,29 +33,51 @@ async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
     ).insertId;
   });
 
-  fastify.get(`${prefix}/:id`, async (req, reply) => {
-    const id = Number((req.params as Todo).id);
-    const [rows] = await connection.query(
-      "SELECT * FROM todo WHERE id = ?",
-      id,
-    );
-    return rows;
-  });
+  fastify.get(
+    `${prefix}/:id`,
+    { preHandler: sessionHandler },
+    async (req, reply) => {
+      const id = (req.params as { id: string }).id;
+      const [rows] = await connection.query(
+        "SELECT * FROM todo WHERE id = ? LIMIT 1",
+        id,
+      );
+      return rows[0] as Todo;
+    },
+  );
 
-  fastify.put(`${prefix}/:id`, TodoValidationSchema, async (req, reply) => {
-    const id = Number((req.params as Todo).id);
-    const todo = req.body as Todo;
-    const [rows] = await connection.query("UPDATE todo SET ? WHERE id = ?", [
-      todo,
-      id,
-    ]);
-    return rows;
-  });
+  fastify.patch(
+    `${prefix}/:id`,
+    { ...TodoValidationSchema },
+    async (req, reply) => {
+      const id = (req.params as { id: string }).id;
+      const todo = req.body as Todo;
+      const result = await connection.query("UPDATE todo SET ? WHERE id = ?", [
+        todo,
+        id,
+      ]);
+      return (
+        result[0] as {
+          fieldCount: number;
+          affectedRows: number;
+          insertId: number;
+        }
+      ).insertId;
+    },
+  );
 
   fastify.delete(`${prefix}/:id`, async (req, reply) => {
-    const id = Number((req.params as Todo).id);
-    const [rows] = await connection.query("DELETE FROM todo WHERE id = ?", id);
-    return rows;
+    const id = (req.params as { id: string }).id;
+    const result = await connection.query("DELETE FROM todo WHERE id = ?", id);
+    return (
+      (
+        result[0] as {
+          fieldCount: number;
+          affectedRows: number;
+          insertId: number;
+        }
+      ).affectedRows > 0
+    );
   });
 
   connection.release();
