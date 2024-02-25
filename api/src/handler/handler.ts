447b56f2b58a18e42d3c32cf2e6b2338edd48daf
Erik Golinelli <erik@codekraft.it>
2024-02-25 23:27:28 +0100

    sessionHandler

--- api/src/handler/handler.ts
+++ api/src/handler/handler.ts
@@ -2,13 +2,19 @@ import { FastifyReply } from "fastify";
 import parseResponse from "./response.js";
 import errorHandler from "./error.js";
 import { ResponseInterface } from "../model/types.js";
+import { verifySession } from "supertokens-node/lib/build/recipe/session/framework/fastify.js";
+
+export const sessionHandler = verifySession({
+  sessionRequired: process.env.NODE_ENV === "production",
+});
 
 export const responseSender = async (
   data: ResponseInterface,
   reply: FastifyReply,
 ): Promise<void> => {
   await errorHandler.reply(data, reply);
-  reply.send(data);
+  console.log(data);
+  return reply.send(data);
 };
 
 const responseHandler = async (
