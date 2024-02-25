51ad6c343866a3eaf54618a056fce844903e49b2
Erik Golinelli <erik@codekraft.it>
2024-02-25 18:23:10 +0100

    the add todo component

--- app/src/components/Form.tsx
+++ app/src/components/Form.tsx
@@ -11,23 +11,31 @@ export function Form({
 }) {
   return (
     <>
-      <form className="w-full max-w-sm mx-auto px-4 py-2">
-        <div className="flex items-center border-b-2 border-teal-500 py-2">
+      <form className="w-full mx-auto px-4 py-2">
+        <div className="flex flex-col items-center border-b-2 border-teal-500 py-2">
           <input
-            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            className="appearance-none bg-transparent border-none w-full text-gray-700 py-1 px-2 leading-tight focus:outline-none"
             type="text"
             placeholder="Add a task"
-            value={task.task_content}
-            onChange={(e) => setTask({ ...task, task_content: e.target.value })}
+            value={task.task_name}
+            onChange={(e) => setTask({ ...task, task_name: e.target.value })}
           />
-          <button
-            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-            type="button"
-            onClick={() => addTask(task)}
-          >
-            Add Task
-          </button>
         </div>
+
+        <textarea
+          className="appearance-none bg-slate-50 border-none w-full h-20 text-gray-700 py-1 px-2 mt-4 leading-tight focus:outline-none"
+          value={task.task_content}
+          placeholder="Description"
+          onChange={(e) => setTask({ ...task, task_content: e.target.value })}
+        />
+
+        <button
+          className="w-full bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white my-2 mr-0 py-1 px-2 rounded"
+          type="button"
+          onClick={() => addTask(task)}
+        >
+          Add Task
+        </button>
       </form>
     </>
   );
