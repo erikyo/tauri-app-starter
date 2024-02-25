9f1eb24d3076e6826a3013bf2921eff09d07c0c6
Erik Golinelli <erik@codekraft.it>
2024-02-25 18:37:10 +0100

    router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,37 @@
 import Tasks from "./components/Tasks.tsx";
 import { Header } from "./components/Header.tsx";
+import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
+import * as reactRouterDom from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { PreBuiltUIList, SuperTokensConfig } from "./config.tsx";
+import { SessionAuth } from "supertokens-auth-react/recipe/session";
+import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react/ui";
+
+SuperTokens.init(SuperTokensConfig);
 
 function App() {
   return (
-    <div className="container m-auto">
-      <div className="max-w-md mx-auto bg-white border border-slate-100 shadow-lg rounded-lg overflow-hidden my-16 p-4">
-        <Header />
-        <Tasks />
+    <div className="flex justify-center items-center my-16 p-4">
+      <div className="mx-auto w-full">
+        <SuperTokensWrapper>
+          <Router>
+            <Routes>
+              {getSuperTokensRoutesForReactRouterDom(
+                reactRouterDom,
+                PreBuiltUIList,
+              )}
+              <Route
+                path="/"
+                element={
+                  <SessionAuth>
+                    <Header />
+                    <Tasks />
+                  </SessionAuth>
+                }
+              />
+            </Routes>
+          </Router>
+        </SuperTokensWrapper>
       </div>
     </div>
   );
