44d2b64c2d96c6db75a14d6c0625f8dc100acb79
Erik Golinelli <erik@codekraft.it>
2024-02-25 18:23:48 +0100

    the Header component

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,9 +1,7 @@
 export function Header() {
   return (
     <div className="p-8">
-      <h1 className="text-gray-800 font-bold text-3xl text-center uppercase">
-        ToDo App
-      </h1>
+      <h1 className="text-gray-800 font-bold text-5xl text-center">Todo App</h1>
     </div>
   );
 }
