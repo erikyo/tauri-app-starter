6504d080ecbcdb835de01496210ac80716fdcfd9
Erik Golinelli <erik@codekraft.it>
2024-02-27 11:34:21 +0100

    server startup script wip

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,19 +1,9 @@
 import appFramework from "./app.js";
 import { FastifyInstance } from "fastify";
-
-import { config } from "dotenv";
-import path from "node:path";
+import { getEnv } from "./utils/index.js";
 
 async function serve() {
-  console.log("Running NODE_ENV: " + process.env.NODE_ENV);
-
-  if (process.env.NODE_ENV === "production") {
-    config({ path: path.resolve(".env.production") });
-  } else if (process.env.NODE_ENV === "docker") {
-    config({ path: path.resolve(".env") });
-  } else {
-    config({ path: path.resolve("../.env") });
-  }
+  getEnv();
 
   // Trigger the application framework to load
   const app = (await appFramework()) as FastifyInstance;
@@ -21,9 +11,8 @@ async function serve() {
   // Start the server
   return await app
     .listen({
-      port: app.config.HTTP_PORT,
-      host:
-        process.env.NODE_ENV === "docker" ? "0.0.0.0" : app.config.HTTP_HOST,
+      port: app.config.API_PORT,
+      host: process.env.NODE_ENV !== "docker" ? app.config.API_HOST : "0.0.0.0",
     })
     .catch((error) => {
       app.log.error(error);
@@ -32,3 +21,5 @@ async function serve() {
 }
 
 serve().catch((error) => console.error(error));
+
+export default serve;
