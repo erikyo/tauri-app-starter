5a4755c2ba5eb00804d10145e24ab54f62460cad
Erik Golinelli <erik@codekraft.it>
2024-02-28 10:37:43 +0100

    env

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,7 +4,13 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    basicSsl({
+      name: "Todo App",
+      domains: ["localhost", "127.0.0.1"],
+    }),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
