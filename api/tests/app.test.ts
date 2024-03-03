1989f5b809ec97757444f21afa10c8993d24d0fb
Erik Golinelli <erik@codekraft.it>
2024-03-03 18:43:14 +0100

    rest api testing

--- api/tests/app.test.ts
+++ api/tests/app.test.ts
@@ -1,11 +1,33 @@
 import { boostrap } from "./helper.js";
+import { beforeAll } from "@jest/globals";
 
 describe("root tests", () => {
-  const app = boostrap();
+  let app: any;
+  beforeAll(async () => {
+    app = await boostrap();
+    return app;
+  });
 
-  test("default root route", async () => {
-    const res = await app.inject("/");
-    console.log(res);
-    expect(res.payload).toEqual("hello");
+  afterAll(async () => {
+    return await app.close();
+  });
+
+  test("server is alive", () => {
+    app
+      .inject({ method: "GET", url: "/" })
+      .then((res: { statusCode: any; payload: any }) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.payload).toEqual("hello");
+      });
+  });
+
+  test("get the list of todos ", () => {
+    app
+      .inject({ method: "GET", url: "/task" })
+      .then((res: { statusCode: any; payload: any }) => {
+        expect(res.statusCode).toBe(200);
+        const taskCount = res.payload.length;
+        expect(taskCount).toBeTruthy();
+      });
   });
 });
