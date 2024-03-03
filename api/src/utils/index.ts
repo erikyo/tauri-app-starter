d1385932d6c954be966b71140582693f5144742e
Erik Golinelli <erik@codekraft.it>
2024-03-03 18:44:26 +0100

    adds the test environment

--- api/src/utils/index.ts
+++ api/src/utils/index.ts
@@ -1,10 +1,12 @@
 import { config } from "dotenv";
 import path from "node:path";
 
-export function getEnv() {
+export function getEnv(env?: string) {
   console.log("Running NODE_ENV: " + process.env.NODE_ENV ?? "development");
 
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "test") {
+    config({ path: path.resolve(".env.test") });
+  } else if (process.env.NODE_ENV === "production") {
     config({ path: path.resolve(".env.production") });
   } else if (process.env.NODE_ENV === "docker") {
     config({ path: path.resolve(".env") });
