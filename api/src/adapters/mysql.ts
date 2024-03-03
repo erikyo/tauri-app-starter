c4d42fec3740113492498f304e1bf9a6f7d59567
Erik Golinelli <erik@codekraft.it>
2024-03-03 18:44:05 +0100

    mysql adapter config (allows testing)

--- api/src/adapters/mysql.ts
+++ api/src/adapters/mysql.ts
@@ -5,8 +5,8 @@ import { FastifyInstance } from "fastify";
 const plugin = async (fastify: FastifyInstance, options: any) => {
   const { config } = fastify as any;
 
-  fastify.register(mysql, {
-    host: process.env.NODE_ENV === "docker" ? "db-app" : config.MYSQL_HOST,
+  const paramenters = {
+    host: "docker" === process.env.NODE_ENV ? "db-app" : config.MYSQL_HOST,
     port: config.MYSQL_PORT,
     user: config.MYSQL_USER,
     password: config.MYSQL_PASSWORD,
@@ -15,7 +15,9 @@ const plugin = async (fastify: FastifyInstance, options: any) => {
     promise: true,
     connectionLimit: 100,
     waitForConnections: true,
-  });
+  };
+
+  fastify.register(mysql, paramenters);
 };
 
 export default fastifyPlugin(plugin);
