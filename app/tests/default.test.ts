f190a60678fc97299bd6c545a42de2910cd2369e
Erik Golinelli <erik@codekraft.it>
2025-11-05 22:08:39 +0100

    app tests updated

--- app/tests/default.test.ts
+++ app/tests/default.test.ts
@@ -1,11 +1,9 @@
-import { describe, expect, test } from "@jest/globals";
+import { expect, test } from 'vitest'
 
-function sum(a: number, b: number): number {
-  return a + b;
+function sum(a, b) {
+    return a + b
 }
 
-describe("sum module", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3)
+})
