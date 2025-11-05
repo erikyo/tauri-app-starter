64223a76c2876ae45406b4f1641692cb6d06b37e
Erik Golinelli <erik@codekraft.it>
2025-11-05 22:10:59 +0100

    app/api auth updated

--- api/src/plugins/auth.ts
+++ api/src/plugins/auth.ts
@@ -5,115 +5,122 @@ import fastifyPlugin from "fastify-plugin";
 
 import supertokens from "supertokens-node";
 import Session from "supertokens-node/recipe/session/index.js";
+import EmailPassword from "supertokens-node/recipe/emailpassword";
 import UserMetadata from "supertokens-node/recipe/usermetadata/index.js";
 import UserRoles from "supertokens-node/recipe/userroles/index.js";
 import Dashboard from "supertokens-node/recipe/dashboard/index.js";
 
-import ThirdPartyEmailPassword from "supertokens-node/recipe/thirdpartyemailpassword/index.js";
+import ThirdParty from "supertokens-node/recipe/thirdparty/index.js";
+import type { TypeInput } from "supertokens-node/types";
 
 import {
-  errorHandler,
-  plugin,
+	errorHandler,
+	plugin,
 } from "supertokens-node/framework/fastify/index.js";
 
-import { type TypeInput } from "supertokens-node/types";
-
 async function auth(server, options) {
-  const recipes = [
-    ThirdPartyEmailPassword.init({
-      providers: [
-        // We have provided you with development keys which you can use for testing.
-        // IMPORTANT: Please replace them with your own OAuth keys for production use.
-        {
-          config: {
-            thirdPartyId: "google",
-            clients: [
-              {
-                clientId: server.config.SUPERTOKENS_3RD_PARTY_GOOGLE_CLIENT_ID,
-                clientSecret:
-                  server.config.SUPERTOKENS_3RD_PARTY_GOOGLE_CLIENT_SECRET,
-              },
-            ],
-          },
-        },
-        {
-          config: {
-            thirdPartyId: "github",
-            clients: [
-              {
-                clientId: server.config.SUPERTOKENS_3RD_PARTY_GITHUB_CLIENT_ID,
-                clientSecret:
-                  server.config.SUPERTOKENS_3RD_PARTY_GITHUB_CLIENT_SECRET,
-              },
-            ],
-          },
-        },
-        {
-          config: {
-            thirdPartyId: "apple",
-            clients: [
-              {
-                clientId: server.config.SUPERTOKENS_3RD_PARTY_APPLE_CLIENT_ID,
-                additionalConfig: {
-                  keyId: server.config.SUPERTOKENS_3RD_PARTY_APPLE_KEY_ID,
-                  privateKey:
-                    server.config.SUPERTOKENS_3RD_PARTY_APPLE_PRIVATE_KEY,
-                  teamId: server.config.SUPERTOKENS_3RD_PARTY_APPLE_TEAM_ID,
-                },
-              },
-            ],
-          },
-        },
-      ],
-    }),
-    // The session module (mandatory)
-    Session.init(),
-    // Store data about each user
-    UserMetadata.init(),
-    // Enable user roles
-    UserRoles.init(),
-    // Enables a dashboard to manage users
-    Dashboard.init(),
-  ];
+	const recipes = [
+        EmailPassword.init(),
+		ThirdParty.init({
+			signInAndUpFeature: {
+				// We have provided you with development keys which you can use for testing.
+				// IMPORTANT: Please replace them with your own OAuth keys for production use.
+				providers: [
+					{
+						config: {
+							thirdPartyId: "google",
+							clients: [
+								{
+									clientId:
+										"1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
+									clientSecret: "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",
+								},
+							],
+						},
+					},
+					{
+						config: {
+							thirdPartyId: "facebook",
+							clients: [
+								{
+									clientId: "TODO",
+									clientSecret: "TODO",
+								},
+							],
+						},
+					},
+					{
+						config: {
+							thirdPartyId: "apple",
+							clients: [
+								{
+									clientId: "4398792-io.supertokens.example.service",
+									additionalConfig: {
+										keyId: "7M48Y4RYDL",
+										privateKey:
+											"-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
+										teamId: "YWQCXGJRJL",
+									},
+								},
+							],
+						},
+					},
+				],
+			},
+		}),
+		// The session module (mandatory)
+		Session.init({
+			exposeAccessTokenToFrontendInCookieBasedAuth: true,
+		}),
+		// Store data about each user
+		UserMetadata.init(),
+		// Enable user roles
+		UserRoles.init(),
+		// Enables a dashboard to manage users
+		Dashboard.init(),
+	];
 
-  const config = {
-    framework: "fastify",
-    supertokens: {
-      // These are the connection details of the app you created on supertokens.com
-      connectionURI:
-        process.env.NODE_ENV === "docker"
-          ? "http://supertokens:3567"
-          : server.config.SUPERTOKENS_CONNECTION_URI,
-      apiKey: server.config.SUPERTOKENS_API_KEY,
-    },
-    appInfo: {
-      // learn more about this on https://supertokens.com/docs/session/appinfo
-      appName: server.config.APP_NAME,
-      apiDomain: server.config.API_DOMAIN,
-      websiteDomain: server.config.APP_DOMAIN,
-      apiBasePath: server.config.SUPERTOKENS_API_BASE_PATH,
-      websiteBasePath: "/auth",
-    },
-    recipeList: recipes,
-  };
+	const config = {
+		framework: "fastify",
+		supertokens: {
+			// These are the connection details of the app you created on supertokens.com
+			connectionURI:
+				["docker", "development"].includes(process.env.NODE_ENV) ||
+				typeof process.env.NODE_ENV === "undefined"
+					? "http://supertokens:3567"
+					: server.config.SUPERTOKENS_CONNECTION_URI,
+			apiKey: server.config.SUPERTOKENS_API_KEY,
+		},
+		appInfo: {
+			// learn more about this on https://supertokens.com/docs/session/appinfo
+			appName: server.config.APP_NAME,
+			apiDomain: server.config.API_DOMAIN,
+			websiteDomain: server.config.APP_DOMAIN,
+			apiBasePath: server.config.SUPERTOKENS_API_BASE_PATH,
+			websiteBasePath: "/auth",
+		},
+		recipeList: recipes,
+	};
 
-  server.register(helmet, { contentSecurityPolicy: false });
+	server.setErrorHandler(errorHandler());
 
-  supertokens.init(config as TypeInput);
+	server.register(helmet, { contentSecurityPolicy: false });
 
-  // we register a CORS route to allow requests from the frontend
-  server.register(cors, {
-    origin: server.config.APP_DOMAIN,
-    allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    credentials: true,
-  });
+	supertokens.init(config as TypeInput);
 
-  server.register(formDataPlugin);
+	// we register a CORS route to allow requests from the frontend
+	server.register(cors, {
+		origin: server.config.APP_DOMAIN,
+		allowedHeaders: ["Content-Type", ...supertokens.getAllCORSHeaders()].join(
+			", ",
+		),
+		methods: "OPTIONS,POST,GET,PUT,DELETE",
+		credentials: true,
+	});
 
-  server.register(plugin);
+	server.register(formDataPlugin);
 
-  server.setErrorHandler(errorHandler());
+	server.register(plugin);
 }
 
 export default fastifyPlugin(auth);
--- app/src/config.tsx
+++ app/src/config.tsx
@@ -1,42 +1,52 @@
-import ThirdPartyEmailPassword, {
-  ActiveDirectory,
-  Apple,
-  Google,
-  LinkedIn,
-} from "supertokens-auth-react/recipe/thirdpartyemailpassword";
-import { ThirdPartyEmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/thirdpartyemailpassword/prebuiltui";
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
 import Session from "supertokens-auth-react/recipe/session";
+import {
+	Facebook,
+	Apple,
+	Google,
+	LinkedIn,
+} from "supertokens-auth-react/recipe/thirdparty";
+import ThirdParty from "supertokens-auth-react/recipe/thirdparty";
+import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
+import { EmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/emailpassword/prebuiltui";
+import type { PreBuiltRecipes } from "supertokens-auth-react/lib/build/ui/types";
 
 export function getApiDomain() {
-  return import.meta.env.API_DOMAIN || "http://127.0.0.1:3001";
+	return import.meta.env.API_DOMAIN || "http://127.0.0.1:3001";
 }
 
 export function getWebsiteDomain() {
-  const websitePort = import.meta.env.APP_PORT || 3000;
-  return import.meta.env.APP_DOMAIN || `http://127.0.0.1:${websitePort}`;
+	const websitePort = import.meta.env.APP_PORT || 3000;
+	return import.meta.env.APP_DOMAIN || `http://127.0.0.1:${websitePort}`;
 }
 
 export const SuperTokensConfig = {
-  appInfo: {
-    appName: import.meta.env.APP_NAME,
-    apiDomain: getApiDomain(),
-    websiteDomain: getWebsiteDomain(),
-  },
-  // recipeList contains all the modules that you want to
-  // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
-  recipeList: [
-    ThirdPartyEmailPassword.init({
-      signInAndUpFeature: {
-        providers: [
-          ActiveDirectory.init(),
-          LinkedIn.init(),
-          Google.init(),
-          Apple.init(),
-        ],
-      },
-    }),
-    Session.init(),
-  ],
+	appInfo: {
+		appName: import.meta.env.APP_NAME,
+		apiDomain: getApiDomain(),
+		websiteDomain: getWebsiteDomain(),
+        apiBasePath: "/auth",
+        websiteBasePath: "/auth",
+	},
+	// recipeList contains all the modules that you want to
+	// use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+	recipeList: [
+		ThirdParty.init({
+			signInAndUpFeature: {
+				providers: [
+					Facebook.init(),
+					LinkedIn.init(),
+					Google.init(),
+					Apple.init(),
+				],
+			},
+		}),
+		EmailPassword.init(),
+		Session.init(),
+	],
 };
 
-export const PreBuiltUIList = [ThirdPartyEmailPasswordPreBuiltUI];
+export const PreBuiltUIList: PreBuiltRecipes = [
+    ThirdPartyPreBuiltUI,
+    EmailPasswordPreBuiltUI,
+];
